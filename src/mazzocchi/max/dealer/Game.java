15eb666879690fdb818a2bd0c62daf42f40c479c
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-07-31 17:20:41 -0400

    Instantiated player list and deck in Game.

--- src/mazzocchi/max/dealer/Game.java
+++ src/mazzocchi/max/dealer/Game.java
@@ -1,11 +1,30 @@
 package mazzocchi.max.dealer;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import mazzocchi.max.dealer.deck.Deck;
+
 public class Game {
-  public Game(int players) {
-    
+
+  private static final int DEFAULT_NUMBER_OF_PLAYERS = 8;
+
+  private List<Player> players;
+  private Deck deck;
+
+  public Game(int number_of_players) {
+    players = new ArrayList<Player>(number_of_players);
+    deck = new Deck();
+  }
+
+  public void playRound() {
+    deck.shuffle();
+
+    // Deal the first two cards
   }
 
   public static void main(String[] args) {
-    System.out.println("Hello world.");
+    Game game = new Game(DEFAULT_NUMBER_OF_PLAYERS);
+    game.playRound();
   }
 }

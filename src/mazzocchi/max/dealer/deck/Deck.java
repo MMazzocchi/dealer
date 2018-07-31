ed51b896ae5b6e22e538ce07c0aa415857b6911c
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-07-31 17:02:32 -0400

    Added basic Deck class.

---
+++ src/mazzocchi/max/dealer/deck/Deck.java
@@ -0,0 +1,26 @@
+package mazzocchi.max.dealer.deck;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Collections;
+
+public class Deck {
+
+  private static final int CARDS_PER_SUIT = 13;
+
+  private List<Card> cards;
+
+  public Deck() {
+    cards = new LinkedList<Card>();
+    for(Suit suit : Suit.values()) {
+      for(int value = 1; value <= CARDS_PER_SUIT; value++) {
+        Card card = new Card(value, suit);
+        cards.add(card);
+      }
+    }
+  };
+
+  public void shuffle() {
+    Collections.shuffle(cards);
+  }
+}

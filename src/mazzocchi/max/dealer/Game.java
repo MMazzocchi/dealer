88c2e534a6e002f13e75957265147f66657516cf
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-07-31 17:36:44 -0400

    Added some printouts.

--- src/mazzocchi/max/dealer/Game.java
+++ src/mazzocchi/max/dealer/Game.java
@@ -8,19 +8,48 @@ import mazzocchi.max.dealer.deck.Deck;
 public class Game {
 
   private static final int DEFAULT_NUMBER_OF_PLAYERS = 8;
+  private static final int USER_INDEX = 0;
 
   private List<Player> players;
+  private int number_of_players;
   private Deck deck;
+  private int dealer_index;
 
   public Game(int number_of_players) {
-    players = new ArrayList<Player>(number_of_players);
-    deck = new Deck();
+    this.number_of_players = number_of_players;
+    this.deck = new Deck();
+    this.dealer_index = 0;
+
+    this.players = new ArrayList<Player>(number_of_players);
+    for(int i=0; i<number_of_players; i++) {
+      this.players.add(new Player());
+    }
   }
 
   public void playRound() {
     deck.shuffle();
 
+    Player user = players.get(USER_INDEX);
+    System.out.println("You are "+user);
+    System.out.println("There are "+number_of_players+" players total.");
+    System.out.println("");
+
+    String dealer_str = players.get(dealer_index).toString() + " is";
+    if(dealer_index == USER_INDEX) {
+      dealer_str = "You are";
+    }
+    System.out.println(dealer_str+" the dealer.");
+
     // Deal the first two cards
+    int next_player = (dealer_index + 1) % number_of_players;
+    for(int card=0; card<2; card++) {
+      for(int i=0; i<number_of_players; i++) {
+        Player player = players.get(next_player);
+        player.dealCard(deck.draw());
+
+        next_player = (next_player + 1) % number_of_players;
+      }
+    }
   }
 
   public static void main(String[] args) {

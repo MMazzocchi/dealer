0773ea2b1d6967be939cc6bbcc9895adb3d8a578
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-07-31 17:23:01 -0400

    Added dealing and clearing methods to Player/Hand.

--- src/mazzocchi/max/dealer/Player.java
+++ src/mazzocchi/max/dealer/Player.java
@@ -1,18 +1,31 @@
 package mazzocchi.max.dealer;
 
+import mazzocchi.max.dealer.deck.Card;
+
 public class Player {
   private static int next_id = 0;
 
   private String name;
   private boolean is_dealer = false;
+  private Hand hand;
 
   public Player() {
     name = "Player #"+next_id;
     next_id++;
+
+    hand = new Hand();
   }
 
   public void setDealer(boolean is_dealer) {
     this.is_dealer = is_dealer;
+  }
+
+  public void dealCard(Card card) {
+    hand.add(card);
+  }
+
+  public void clearHand() {
+    hand.clear();
   }
 
   @Override

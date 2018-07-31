aa47174f70731c2cfd20611cbd0482b87506dc2c
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-07-31 17:04:37 -0400

    Added Dealer boolean to Player.

--- src/mazzocchi/max/dealer/Player.java
+++ src/mazzocchi/max/dealer/Player.java
@@ -2,14 +2,24 @@ public class Player {
   private static int next_id = 0;
 
   private String name;
+  private boolean is_dealer = false;
 
   public Player() {
     name = "Player #"+next_id;
     next_id++;
   }
 
+  public void setDealer(boolean is_dealer) {
+    this.is_dealer = is_dealer;
+  }
+
   @Override
   public String toString() {
-    return name;
+    if(is_dealer) {
+      return name+" (dealer)";
+
+    } else {
+      return name;
+    }
   }
 }

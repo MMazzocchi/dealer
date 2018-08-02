fb2ff563ffa2f714d3fb88738f3063e3decc8070
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-08-02 17:56:40 -0400

    Added constructor, advance, and toString methods to Stage.

--- src/mazzocchi/max/dealer/Stage.java
+++ src/mazzocchi/max/dealer/Stage.java
@@ -1,9 +1,37 @@
 package mazzocchi.max.dealer;
 
 enum Stage {
-  NEW_ROUND,
-  DEAL,
-  FLOP,
-  TURN,
-  RIVER
+  NEW_ROUND("New Round"),
+  DEAL("Deal"),
+  FLOP("Flop"),
+  TURN("Turn"),
+  RIVER("River");
+
+  private String name;
+
+  private Stage(String name) {
+   this.name = name;
+  }
+
+  public Stage next() {
+    switch(this) {
+      case NEW_ROUND:
+        return DEAL;
+      case DEAL:
+        return FLOP;
+      case FLOP:
+        return TURN;
+      case TURN:
+        return RIVER;
+      case RIVER:
+        return NEW_ROUND;
+    }
+
+    return NEW_ROUND;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
 };

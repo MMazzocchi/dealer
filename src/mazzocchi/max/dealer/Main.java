5f01069969db5d810b9057f4dcf6fe802944b4f8
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-08-02 17:36:55 -0400

    Moved pausing and printing code into Main, now using advance() method.

--- src/mazzocchi/max/dealer/Main.java
+++ src/mazzocchi/max/dealer/Main.java
@@ -1,8 +1,24 @@
 package mazzocchi.max.dealer;
 
+import java.io.IOException;
+
 public class Main {
 
   private static final int DEFAULT_NUMBER_OF_PLAYERS = 8;
+
+  /**
+   * Print a message and block until the user presses Enter.
+   */
+  private static void pause() {
+    System.out.println("Press <Enter> to continue.");
+    try {
+      System.in.read();
+
+    } catch(IOException ioe) {
+      System.err.println("An error occured while waiting for user input"+
+        ioe.getMessage());
+    }
+  }
 
   /**
    * Start a new Game with the default number of players, and play rounds
@@ -11,8 +27,57 @@ public class Main {
   public static void main(String[] args) {
     Game game = new Game(DEFAULT_NUMBER_OF_PLAYERS);
 
+    //Player user = players.get(USER_INDEX);
+    //System.out.println("You are "+user+".");
+
     while(true) {
-      game.playRound();
+      Stage stage = game.advance();
+
+      switch(stage) {
+        case NEW_ROUND:
+          System.out.println("================== NEW ROUND ==================");
+          System.out.println("");
+          System.out.println("There are "+DEFAULT_NUMBER_OF_PLAYERS+
+            " player(s) total.");
+          System.out.println("");
+
+/*
+          if(dealer_index == USER_INDEX) {
+            System.out.println("You are the dealer.");
+
+          } else {
+            Player dealer = players.get(dealer_index);
+            System.out.println(dealer+" is the dealer.");
+          }
+*/
+          System.out.println("");
+          pause();
+          break;
+
+        case DEAL:
+          System.out.println("Your hand:");
+          //user.printHand();
+          pause();
+          break;
+
+        case FLOP:
+          System.out.println("Flop:");
+          //round.printCardsOnTable();
+          pause();
+          break;
+
+        case TURN:
+          System.out.println("Turn:");
+          //round.printCardsOnTable();
+          pause();
+          break;
+
+        case RIVER:
+          System.out.println("River: ");
+          //round.printCardsOnTable();
+          pause();
+          break;
+      }
     }
   }
 }

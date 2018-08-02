09afac8bbafb4fe03ccf6f43701958845c2e6068
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-08-02 17:58:38 -0400

    Printed the stage at the beginning of the stage in Main.

--- src/mazzocchi/max/dealer/Main.java
+++ src/mazzocchi/max/dealer/Main.java
@@ -43,17 +43,15 @@ public class Main {
 
     while(true) {
       Stage stage = game.advance();
+      System.out.println(stage+":");
 
       switch(stage) {
         case NEW_ROUND:
-          System.out.println("================== NEW ROUND ==================");
-          System.out.println("");
-
           Player dealer = game.getDealer();
           if(user == dealer) {
-            System.out.println("You are the dealer.");
+            System.out.println("  You are the dealer.");
           } else {
-            System.out.println(dealer+" is the dealer.");
+            System.out.println("  "+dealer+" is the dealer.");
           }
 
           System.out.println("");
@@ -61,27 +59,24 @@ public class Main {
           break;
 
         case DEAL:
-          System.out.println("Your hand:");
+          System.out.println("  Your hand:");
           for(Card card : user.getHand()) {
-            System.out.println("  "+card);
+            System.out.println("    "+card);
           }
           pause();
           break;
 
         case FLOP:
-          System.out.println("Flop:");
           printCardsOnTable(game);
           pause();
           break;
 
         case TURN:
-          System.out.println("Turn:");
           printCardsOnTable(game);
           pause();
           break;
 
         case RIVER:
-          System.out.println("River: ");
           printCardsOnTable(game);
           pause();
           break;
